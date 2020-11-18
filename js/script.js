9cc9862eb4ffdb6e87054de371fcf5e800bf0477
Riccardo Ferrari <richi.f93@gmail.com>
2020-11-18 06:32:01 +0100

    sostituzione array con semplice variabile

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 // Scrivi un programma che stampi i numeri da 1 a 100,
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-var listaNumeri = [];
+var numero;
 // var numero = 1;
 for (i = 1; i < 101; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
-    listaNumeri.push( "FizzBuzz")
+    numero = "FizzBuzz";
   } else if (i % 5 == 0) {
-    listaNumeri.push( "Buzz")
+  numero = "Buzz";
   } else if (i % 3 == 0 ) {
-    listaNumeri.push( "Fizz")
+    numero = "Fizz";
   } else {
-    listaNumeri.push(i);
+    numero = i;
   }
+  document.getElementById('buzzer').innerHTML += numero + "<br>";
 }
-// listaNumeri.shift();
-console.log(listaNumeri);
